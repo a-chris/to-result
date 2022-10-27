b8fa87d47a91382f1c835d946d1b57ff2a327c29
a-chris <a.christian.toscano@gmail.com>
2022-10-28 00:59:30 +0200

    convert args to keys, add local on_error, add tests

--- lib/to-result.rb
+++ lib/to-result.rb
@@ -5,6 +5,12 @@ module ToResultMixin
 
   class Configuration
     attr_accessor :on_error
+
+    def on_error=(value)
+      raise TypeError.new('on_error is expected to be a callable object') unless value.respond_to?(:call)
+
+      @on_error = value
+    end
   end
 
   @@configuration = Configuration.new
@@ -19,27 +25,37 @@ module ToResultMixin
   #
   # ToResult executes a block of code and returns Success or Failure.
   # All exceptions inherited from StandardError are catched and
-  # converted to Failure or you can pass a list of exceptions to catch.
+  # converted to Failure or you can pass a custom list of exceptions
+  # to catch using `only`.
   #
-  # @param [Array<Class>] exceptions
-  # @param [Proc] &f
+  # Passing the `on_error` block overrides the global on_error defined in to the Configuration,
+  # it is possible to pass `nil` to not execute a block when an error is catched.
+  #
+  #
+  # @param [Array<Class>] only is the array of Exception to catch
+  # @param [Proc|Method] on_error is the function to be executed in case of error. It overrides the global on_error.
+  # @param [Proc] &f is the block to run
   #
   # @return [Success]
   # @return [Failure]
   #
-  def ToResult(exceptions = [StandardError], &f)
-    Try.run(
-      exceptions,
+  def ToResult(only: [StandardError], **args, &f)
+    # on_error included in args so we can distinguish when it's passed but it's nil
+    # from when it's not passed at all
+    on_error ||= args.key?(:on_error) ? args[:on_error] : @@configuration.on_error
+
+    f_wrapper =
       Proc.new do
         f.call
       rescue Dry::Monads::Do::Halt => e
         error = e.result
-        @@configuration.on_error.call(error) if @@configuration.on_error.respond_to?(:call)
+        on_error.call(error) if on_error.respond_to?(:call)
         return error
-      rescue *exceptions => e
-        @@configuration.on_error.call(e) if @@configuration.on_error.respond_to?(:call)
+      rescue *only => e
+        on_error.call(e) if on_error.respond_to?(:call)
         raise e
       end
-    ).to_result
+
+    Try.run(only, f_wrapper).to_result
   end
 end
--- tests/to_result_test.rb
+++ tests/to_result_test.rb
@@ -36,12 +36,12 @@ class ToResultTest < Minitest::Test
 
   def test_exception_included_in_exceptions_list
     expected = ArgumentError.new(@value)
-    assert ToResult([ArgumentError]) { raise expected } == Failure(expected)
+    assert ToResult(only: [ArgumentError]) { raise expected } == Failure(expected)
   end
 
   def test_exception_not_included_in_exceptions_list
     expected = NameError.new(@value)
-    assert_raises(NameError) { ToResult([ArgumentError]) { raise expected } }
+    assert_raises(NameError) { ToResult(only: [ArgumentError]) { raise expected } }
   end
 
   def test_yield_failure
@@ -56,9 +56,7 @@ class ToResultTest < Minitest::Test
     assert ToResult { yield expected } == expected
   end
 
-  def test_on_error
-    FakeLogger.expects(:log_error).once
-
+  def setup_global_on_error
     # creating a clean room just for testing purpose
     clean_room = Class.new(Object)
     clean_room.new.instance_eval do
@@ -66,8 +64,36 @@ class ToResultTest < Minitest::Test
         c.on_error = Proc.new { FakeLogger.log_error }
       end
     end
+  end
+
+  def test_global_on_error
+    setup_global_on_error
+
+    FakeLogger.expects(:log_error).once
 
     expected = StandardError.new(@value)
     assert ToResult { raise expected } == Failure(expected)
+  end
+
+  def test_local_on_error_overrides_global
+    setup_global_on_error
+
+    FakeLogger.expects(:log_error).once
+
+    local_on_error = Proc.new { FakeLogger.log_error }
+
+    expected = StandardError.new(@value)
+    assert ToResult(on_error: local_on_error) { raise expected } == Failure(expected)
+  end
+
+  def test_local_on_error_overrides_global_nil
+    setup_global_on_error
+
+    FakeLogger.expects(:log_error).never
+
+    local_on_error = nil
+
+    expected = StandardError.new(@value)
+    assert ToResult(on_error: local_on_error) { raise expected } == Failure(expected)
   end
 end
